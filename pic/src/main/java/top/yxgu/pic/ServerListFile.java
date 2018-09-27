6f3d471a4eaec1a701c7ab831329289ed645da56
blc <blianchen@qq.com>
2018-09-27 17:47:40 +0800

    server list

---
+++ pic/src/main/java/top/yxgu/pic/ServerListFile.java
@@ -0,0 +1,51 @@
+package top.yxgu.pic;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerListFile {
+
+    private static String fileName = "srv.conf";
+    private static String charset = "UTF-8";
+
+    private static Context context;
+    private static List<String> list = new ArrayList<>();
+
+    public static void init(Context cxt) {
+        context = cxt;
+        try (FileInputStream fi = context.openFileInput(fileName)) {
+            int byteNum = fi.available();
+            byte[] bytes = new byte[byteNum];
+            int num = fi.read(bytes);
+            if (num > 0) {
+                String str = new String(bytes, charset);
+                String[] arr = str.split("\n");
+                for (String v : arr) {
+                    list.add(v);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void add(String s) {
+        list.add(s);
+        try (FileOutputStream fo = context.openFileOutput(fileName, Context.MODE_PRIVATE)) {
+            for (String v: list) {
+                fo.write(v.getBytes(charset));
+                fo.write('\n');
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> get() {
+        return list;
+    }
+}
